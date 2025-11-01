ca1c71737e6c91bafa577cfc4663805305932605
Gabegi <gabriel_pirastru@hotmail.fr>
2025-11-01 20:42:06 +0600

    EFCore

--- src/MinimalAPI/Program.cs
+++ src/MinimalAPI/Program.cs
@@ -1,18 +1,31 @@
 using FluentValidation;
 using Infrastructure;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Register services
-builder.Services.AddSingleton<IRandomProductRepository, RandomProductRepository>();
-builder.Services.AddSingleton<ICustomerRepository, CustomerRepository>();
-builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
+// Register DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register repositories
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IRandomProductRepository, RandomProductRepository>();
 
 // Register validators
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
+
+// Apply migrations
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Map endpoints
 app.MapProducts();
